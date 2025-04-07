05713b5840b28910139fed8022e341c76c1b4842
radhika-modi12 <radhika.h.modi11@gmail.com>
2025-04-07 20:13:52 +0530

    upload code

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,19 @@
 export default function About() {
-    return(
-        <p>About page</p>
-    )
+  return (
+    <div className="about-container">
+      <div className="about-content">
+        <h1 className="about-data">About Me</h1>
+        <p>about data</p>
+      </div>
+      <div className="about-content">
+        <h1 className="about-data">Skills</h1>
+        <p>web developer</p>
+        <p>web design</p>
+      </div>
+      <div className="about-content">
+        <h1 className="about-data">Interests</h1>
+        <p>designing</p>
+      </div>
+    </div>
+  );
 }
---
+++ src/app/projects/page.js
@@ -0,0 +1,19 @@
+export default function Projects() {
+    return(
+        <div className="blog-detail">
+        <h1>project page</h1>
+        <div className="blog-container">
+            <div className="blog-content">
+                <h1 className="blog-title">blog1</h1>
+                <p className="blog-desc">blog description</p>
+                <button className="blog-btn">view blog</button>
+            </div>
+            <div className="blog-content">
+                <h1 className="blog-title">blog 2</h1>
+                <p className="blog-desc">blog description</p>
+                <button className="blog-btn">view blog</button>
+            </div>
+        </div>
+        </div>
+    )
+}
