29ad75b935907bb4ca69be6839574e13a2a7f102
radhika-modi12 <radhika.h.modi11@gmail.com>
2025-04-28 20:09:48 +0530

    upload api

--- src/app/api/blogs/route.js
+++ src/app/api/blogs/route.js
@@ -16,3 +16,48 @@ export async function POST(request) {
     );
   }
 }
+
+export async function GET(request) {
+  try {    
+    await connectMongoDB();
+    const blogData =await blogDetail.find()
+    return NextResponse.json({ message: "blog get",list:blogData }, { status: 201 });
+  } catch (error) {
+    console.log({ error });
+    return NextResponse.json(
+      { error: "Failed to get blog" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(request) {
+  try {    
+    const {id,name,email,phone} =await request.json()
+    await connectMongoDB();
+    const user_set ={name,email,phone}
+    const blogData =await blogDetail.findByIdAndUpdate(id,user_set,{new:true})
+    return NextResponse.json({ message: "blog update",list:blogData }, { status: 201 });
+  } catch (error) {
+    console.log({ error });
+    return NextResponse.json(
+      { error: "Failed to update blog" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(request) {
+  try {    
+    const {id} =await request.json()
+    await connectMongoDB();    
+    const blogData =await blogDetail.findByIdAndDelete(id)
+    return NextResponse.json({ message: "blog delete",list:blogData }, { status: 201 });
+  } catch (error) {
+    console.log({ error });
+    return NextResponse.json(
+      { error: "Failed to delete blog" },
+      { status: 500 }
+    );
+  }
+}
